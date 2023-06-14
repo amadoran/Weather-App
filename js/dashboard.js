a98a0d551253018baa0f85901a18ea3565bf54b3
amadoran <afamador@espol.edu.ec>
2023-06-14 15:01:31 -0500

    Guia 10

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,67 @@
-const getWeatherApi = async () => {
-    let weatherProm = await fetch('https://api.open-meteo.com/v1/forecast?latitude=-2.15&longitude=-79.97&hourly=temperature_2m,precipitation_probability&current_weather=true&timezone=auto')
-    let weather = await weatherProm.json()
-    let timezone = weather["timezone"]
-    let elemtz = document.getElementById("timezone")
-    elemtz.textContent = timezone
+let plot = (data) => {
+    const ctxLine = document.getElementById("myChart")
+    const ctxBar = document.getElementById("chart-bar")
+
+    const datasetLine = {
+        labels: data.hourly.time,
+        datasets: [{
+            label: 'Temperatura semanal',
+            data: data.hourly.temperature_2m,
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    }
+    const datasetBar = {
+        labels: data.daily.time,
+        datasets: [{
+            label: 'Indice UV',
+            data: data.daily.uv_index_max,
+            borderColor: 'rgb(75, 192, 192)',
+            backgroundColor: [
+                'rgb(75, 192, 192, 0.2)',
+                'rgb(75, 192, 192, 0.2)',
+                'rgb(75, 192, 192, 0.2)',
+                'rgb(75, 192, 192, 0.2)',
+                'rgb(75, 192, 192, 0.2)',
+                'rgb(75, 192, 192, 0.2)',
+                'rgb(75, 192, 192, 0.2)'
+            ],
+            borderWidth: 1
+        }]
+    }
+
+    const configLine = {
+        type: 'line',
+        data: datasetLine
+    }
+    const configBar = {
+        type: 'bar',
+        data: datasetBar
+    }
+
+    const chartLine = new Chart(ctxLine, configLine)
+    const chartBar = new Chart(ctxBar, configBar)
 }
 
-getWeatherApi()
+let load = (data) => {
+    let timezone = data["timezone"]
+    let elemtz = document.getElementById("timezone")
+    elemtz.textContent = timezone
+    plot(data)
+}
+
+let meteo = localStorage.getItem("meteo");
+
+(
+    async function(){
+        if (meteo == null){
+            let weatherProm = await fetch('https://api.open-meteo.com/v1/forecast?latitude=-2.15&longitude=-79.97&hourly=temperature_2m&daily=uv_index_max&current_weather=true&timezone=auto')
+            let weather = await weatherProm.json()
+            load(weather)
+            localStorage.setItem('meteo', JSON.stringify(weather))
+        } else{
+            load(JSON.parse(meteo))
+        }
+    }
+)();
